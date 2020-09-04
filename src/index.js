8dde8cbaf0b735f8402f8cf55529964963a324c9
Sigal Kroitoro <kroitorosigal@gmail.com>
2020-09-04 23:40:33 +0300

    created thankYou page and stats page, switching between them dosent work well yet.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,35 @@ function generateYearList() {
   }
   const yearList = numList.map((number,index)=>
     <option key={index.toString()}> {number} </option> );
-  yearList.splice(0,0,<option key='pick'> Pick your birth year </option>); //use unshift instead
+  yearList.unshift(<option key='pick' > Pick your birth year </option>);
   return yearList;
+}
+
+function headerRender(titleText) {
+  return (
+    <div>
+      <img src={logo} alt="Why website logo" />
+      <h1>{titleText}</h1>
+    </div>
+  )
+}
+
+function statsPage(companyName) {
+  return(
+    <div>
+      <h1> here you will see the stats about {companyName}: </h1>
+      <ul>
+        <li> sumOfComplaints(companyName) </li>
+        <li> sumDistinctClients(companyName) </li>
+        <li> topFiveCompanies() </li>
+        <li> listOfComplaints(companyName) </li>
+      </ul>
+{/*      {sumOfComplaints(companyName)}
+      {sumDistinctClients(companyName)}
+      {topFiveCompanies()}
+      {listOfComplaints(companyName)}*/}
+    </div>
+  )
 }
 
 class ClientInputForm extends React.Component {
@@ -23,10 +50,10 @@ class ClientInputForm extends React.Component {
       clientPhone: '',
       clientEmail: '',
       clientYearOfBirth: '',
-      clientComplaint: '',
       companyName: '',
       complaintContent: '',
-//      userNickName: 'Bro', //for easter egg;
+
+      pageName: 'inputForm',
     } ;
     this.handleInputChange=this.handleInputChange.bind(this);
     this.handleSubmit= this.handleSubmit.bind(this);
@@ -64,9 +91,12 @@ class ClientInputForm extends React.Component {
   }
 
   handleSubmit(){
-    if (this.validateForm()){
+    if (this.validateForm()) {
       const jsonComplaint = this.toJson(this.state);
-      return jsonComplaint; //to be continued..
+      this.setState({pageName: 'thankYou'}); //jumps back to 'inputForm right after alternating to 'thankYou' --bug
+/*
+      return jsonComplaint;
+*/
     }
   }
 
@@ -81,59 +111,111 @@ class ClientInputForm extends React.Component {
   }
 
   render() {
-    return(
-      <div>
-        <img src={logo} alt="Why website logo" />
-        <h1>
-          We Hear You, Bro. </h1>
-        <form onSubmit={this.handleSubmit} method="post"> {/*need to learn more about onSubmit structure*/}
-          <div id="userInfoSection">
-            <input
-              required
-              name= "clientName"
-              type="text"
-              value = {this.state.clientName}
-              placeholder = {'Your name*'}
-              onChange = {this.handleInputChange}
-            />
-            <input
-              name= "clientEmail"
-              type="text"
-              value = {this.state.clientEmail}
-              placeholder = {'Your email address'}
-              onChange = {this.handleInputChange}
-            />
-            <input
-              name= "clientPhone"
-              type="text"
-              value = {this.state.clientPhone}
-              placeholder = {'Your phone number'}
-              onChange = {this.handleInputChange}
-            />
-            <select
-              name="clientYearOfBirth">
-              {generateYearList()}
-            </select>
+    switch (this.state.pageName) {
+      case 'inputForm':
+        return(
+          <div>
+            {headerRender("We Hear You, Bro.")}
+            <form onSubmit={this.handleSubmit} > {/*need to learn more about onSubmit structure*/}
+              <div id="userInfoSection" >
+                <input
+                  required
+                  name= "clientName"
+                  value = {this.state.clientName}
+                  placeholder = {'Your name*'}
+                  onChange = {this.handleInputChange}
+                />
+                <input
+                  name= "clientEmail"
+                  value = {this.state.clientEmail}
+                  placeholder = {'Your email address'}
+                  onChange = {this.handleInputChange}
+                />
+                <input
+                  name= "clientPhone"
+                  value = {this.state.clientPhone}
+                  placeholder = {'Your phone number'}
+                  onChange = {this.handleInputChange}
+                />
+                <select
+                  name="clientYearOfBirth">
+                  {generateYearList()}
+                </select>
+              </div>
+              <textarea
+                name= "companyName"
+                required
+                value = {this.state.companyName}
+                placeholder = {'Who pissed you off, dude? (Company Name OR any other identifier)'}
+                onChange = {this.handleInputChange}
+              />
+              <textarea
+                id="complaint"
+                name= "clientComplaint"
+                value = {this.state.clientComplaint}
+                placeholder = {'Type your complaint in free text here'}
+                onChange = {this.handleInputChange}
+              />
+              <input id="submit" type="submit" value="Submit complaint" />
+            </form>
           </div>
-            <textarea
-              name= "companyName"
-              required
-              type="text"
-              value = {this.state.companyName}
-              placeholder = {'Who pissed you off, dude? (Company Name OR any other identifier)'}
-              onChange = {this.handleInputChange}
-            />
-            <textarea
-              id="complaint"
-              name= "clientComplaint"
-              type="text"
-              placeholder = {'Type your complaint in free text here'}
-              onChange = {this.handleInputChange}
-            />
-            <input id="submit" type="submit" value="Submit complaint"/>
-        </form>
-      </div>
-    )
+        );
+        break;
+      case 'thankYou':
+        return(
+          <div>
+            {headerRender("Thanks! we'll be in touch")}
+            <form>
+              <div id="userInfoSection" >
+                <input
+                  name= "clientName"
+                  value = {'Your name*'}
+                  disabled = {true}
+                />
+                <input
+                  name= "clientEmail"
+                  value = {'Your email address'}
+                  disabled = {true}
+                />
+                <input
+                  name= "clientPhone"
+                  value = {'Your phone number'}
+                  disabled = {true}
+                />
+                <select
+                  name="clientYearOfBirth"
+                  disabled = {true}
+                  >
+                  {<option> Pick your birth year </option>}
+                </select>
+              </div>
+              <textarea
+                name= "companyName"
+                value = {'I know man, that company sucks...'}
+                disabled = {true}
+              />
+              <textarea
+                id="complaint"
+                name= "clientComplaint"
+                value = {'Type your complaint in free text here'}
+                disabled = {true}
+              />
+              <input id="submit" type="submit" value="Submit complaint" disabled = {true} />
+            </form>
+          </div>
+        );
+        break;
+      case 'stats':
+        return(
+          <div>
+              {headerRender()}
+              {statsPage(this.state.companyName)}
+          </div>
+        );
+        break;
+      default:
+        return ({headerRender})
+    }
   }
 }
 
